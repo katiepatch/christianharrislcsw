0fdac7366c85666fdd7e700e7c2f52be63ec62d0
cocoswank <patchkatie@gmail.com>
2016-03-20 14:25:33 -0500

    Added navigation controller for active links

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,22 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
 
+angular.module('myApp.controllers', []).
+  controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.$location = $location;
+  }])
+  .controller('MyCtrl1', [function() {
   }])
   .controller('MyCtrl2', [function() {
-
+  }])
+  .controller('MyCtrl3', [function() {
+  }])
+  .controller('MyCtrl4', [function() {
+  }])
+  .controller('MyCtrl5', [function() {
+  }])
+  .controller('MyCtrl6', [function() {
+  }])
+  .controller('MyCtrl7', [function() {
   }]);
