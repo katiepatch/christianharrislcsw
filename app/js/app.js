f39184c576f2a19260132ff39964bbbed7f0f95d
cocoswank <patchkatie@gmail.com>
2016-03-20 13:55:57 -0500

    Reworked file structure, set up partials

---
+++ app/js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/psychotherapy', {templateUrl: 'partials/psychotherapy.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/families', {templateUrl: 'partials/families.html', controller: 'MyCtrl3'});
+  $routeProvider.when('/couples', {templateUrl: 'partials/couples.html', controller: 'MyCtrl4'});
+  $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'MyCtrl5'});
+  $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'MyCtrl6'});
+  $routeProvider.when('/resources', {templateUrl: 'partials/resources.html', controller: 'MyCtrl7'});
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
